379b42d0296b5dcf7dc6fcd68f204ae8a54a4750
Eric Harvey <swimleft@gmail.com>
2021-08-20 13:24:50 -0700

    first

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
+import StreamDelete from './streams/StreamDelete'
+import StreamEdit from './streams/StreamEdit'
+import StreamCreate from './streams/StreamCreate'
+import StreamShow from './streams/StreamShow'
+import StreamList from './streams/StreamList'
+import Header from './Header'
+
+
+const App = () => {
+    
+    return(
+        <div className="ui container">
+            
+            <BrowserRouter>
+            <div>     
+                <Header />      
+                <Route path="/" exact component={StreamList}/>
+                <Route path="/stream/new" exact component={StreamCreate}/>
+                <Route path="/stream/edit" exact component={StreamEdit}/>
+                <Route path="/stream/delete" exact component={StreamDelete}/>
+                <Route path="/stream/show" exact component={StreamShow}/>
+            </div>
+            </BrowserRouter>
+        </div>
+    )
+}
+
+export default App
